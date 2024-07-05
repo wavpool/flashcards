44f7f304fa2c11ac2bc5dc7542520d2af9fe8f79
Willem Pol <wpol.edu@gmail.com>
2024-07-05 06:49:16 -0600

    changed the scope of variables in deck test

--- test/card-test.js
+++ test/card-test.js
@@ -1,7 +1,12 @@
 const chai = require('chai');
 const expect = chai.expect;
+const data = require('../src/data')
+const sampleData = data.prototypeData
 
 const { createCard } = require('../src/card');
+
+const card1 = createCard(1, "What allows you to define a set of related information using key-value pairs?", ["object", "array", "function"], "object")
+const firstSampleCard = sampleData[0]
 
 describe('card', function() {
   it('should be a function', function() {
@@ -16,4 +21,8 @@ describe('card', function() {
     expect(card.answers).to.deep.equal(['object', 'array', 'function']);
     expect(card.correctAnswer).to.equal('object');
   });  
+
+  it('should have formatting consistent with sample data', () => {
+    expect(card1).to.deep.equal(firstSampleCard)
+  })
 });
